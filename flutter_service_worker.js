90cd5989758879f9060102ea0b494ca978dc3ccc
Chen Xiao <chenxiao770117@gmail.com>
2023-05-03 10:09:37 +0800

    upate

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "199cfcbf5ce38c82098345301bfa7f50",
-"index.html": "d0eccfe7a2c23a8689e06531c799b2cc",
-"/": "d0eccfe7a2c23a8689e06531c799b2cc",
-"main.dart.js": "2e4418fb388339ef8c8b500574f517c9",
+"index.html": "5388f705c5fedb8873621ade9ec50434",
+"/": "5388f705c5fedb8873621ade9ec50434",
+"main.dart.js": "95573e44f905887f221d31a8cb7b2d7f",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
